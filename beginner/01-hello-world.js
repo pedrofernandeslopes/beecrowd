f3b37e2435511cb4bb72989521e490aa4033d12e
pedro <pedroflopes1710@gmail.com>
2023-07-29 09:33:52 -0300

    Hello World

--- beginner/01-hello-world.js
+++ beginner/01-hello-world.js
@@ -1,1 +1,2 @@
+//Your first program in any programming language is usually "Hello World!". In this first problem all you have to do is print this message on the screen.
 console.log('Hello World!');
