581518d17ce15b4356c78ba18e32c105f9b00e4e
pedro <pedroflopes1710@gmail.com>
2023-07-29 09:33:22 -0300

    Extremely Basic

---
+++ beginner/02-extremely-basic.js
@@ -0,0 +1,9 @@
+//Faça um programa que leia dois valores inteiros, no caso para variáveis A e B. A seguir, calcule a soma entre elas e atribua à variável X.
+
+var A = parseInt(lines.shift());
+var B = parseInt(lines.shift());
+X = A + B;
+
+console.log(`X = ${X}`);
+
+
