380214c9f2feaf69f2f43d8c6b9ece14de334336
pedro <pedroflopes1710@gmail.com>
2023-07-31 08:04:41 -0300

    Extremely Basic

--- beginner/02-extremely-basic.js
+++ beginner/02-extremely-basic.js
@@ -1,9 +1,10 @@
 //Faça um programa que leia dois valores inteiros, no caso para variáveis A e B. A seguir, calcule a soma entre elas e atribua à variável X.
 
+var lines = input.split('\n');
 var A = parseInt(lines.shift());
-var B = parseInt(lines.shift());
-X = A + B;
+let B = parseInt(lines.shift());
 
+X = A + B;
 console.log(`X = ${X}`);
 
 
