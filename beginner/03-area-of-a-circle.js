7d9bc446bd9e0e95b93a9fd0f77221a4f6bb2c32
pedro <pedroflopes1710@gmail.com>
2023-07-31 08:02:37 -0300

    Area of a Circle

--- beginner/03-area-of-a-circle.js
+++ beginner/03-area-of-a-circle.js
@@ -1,3 +1,11 @@
 // The formula to calculate the area of a circumference is defined as A = π . R2. Considering to this problem that π = 3.14159:
 // Calculate the area using the formula given in the problem description.
 
+var lines = input.split('\n');
+
+let R = parseFloat(lines.shift());
+const n = 3.14159;
+
+A = (n * R * R).toFixed(4);
+console.log(`A = {A}`);
+
