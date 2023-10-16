b344f1a296339f2a30cd9e7e21171ea7fba38cb2
Pedro Fernandes Lopes <pedroflopes1710@gmail.com>
2023-10-16 14:10:12 -0300

    1007 Difference

---
+++ Java/beginner/08-Difference/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+
+        //Declaration
+        Scanner scanner = new Scanner(System.in);
+
+        //Input
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        int C = scanner.nextInt();
+        int D = scanner.nextInt();
+
+        //Processing
+        int DIFERENCA = (A * B - C * D);
+
+        //Output
+        System.out.println("DIFERENCA = " + DIFERENCA);
+        scanner.close();
+    }
+}
