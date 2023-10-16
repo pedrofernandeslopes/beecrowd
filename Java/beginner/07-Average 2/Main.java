6c6fbc99cf4e92f96a6dcad884335dc82260c82c
Pedro Fernandes Lopes <pedroflopes1710@gmail.com>
2023-10-16 13:51:08 -0300

    1006 Average 2

---
+++ Java/beginner/07-Average 2/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        //Declaration
+        Scanner scanner = new Scanner(System.in);
+        double weightA = 2;
+        double weightB = 3;
+        double weightC = 5;
+
+        //Input
+        double A = scanner.nextDouble();
+        double B = scanner.nextDouble();
+        double C = scanner.nextDouble();
+
+        //Processing
+        double average = ((A * weightA + B * weightB + C * weightC) / 10 );
+
+        //Output
+        System.out.printf("MEDIA = %.1f%n", average);
+        scanner.close();
+    }
+}
