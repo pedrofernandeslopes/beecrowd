8e0a497593efe74523bf664fd17d534e5c204d93
Pedro Fernandes Lopes <pedroflopes1710@gmail.com>
2023-10-19 23:07:28 -0300

    1008 Salary

---
+++ Java/beginner/09-Salary/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+
+        //Declaration
+        Scanner scanner = new Scanner(System.in);
+
+        //Input
+        double number = scanner.nextDouble();
+        double salary = scanner.nextDouble();
+        double time = scanner.nextDouble();
+
+        //Processing
+        double salary = 
+
+        //Output
+        System.out.println();
+        scanner.close();
+    }
+}
