924f803e8664883a124eb70da82f5271ca0cfe25
Pedro Fernandes Lopes <pedroflopes1710@gmail.com>
2023-10-20 23:09:41 -0300

    1009 Salary with Bonus

---
+++ Java/beginner/10-Salary with Bonus/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Declaration
+        Scanner scanner = new Scanner(System.in);
+
+        //Input
+        String name = scanner.nextLine();
+        double fixedSalary  = scanner.nextDouble();
+        double totalSales = scanner.nextDouble();
+        
+
+        //Processing
+        double comission = totalSales * 0.15;
+        double totalSalary = fixedSalary + comission;
+
+        //Output
+        System.out.printf("TOTAL = R$ %.2f\n", totalSalary);
+
+        scanner.close();
+    }
+}
