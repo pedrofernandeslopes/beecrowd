a25d265a6eb4b5dedc5503c3f845482d6b3505be
Pedro Fernandes Lopes <pedroflopes1710@gmail.com>
2023-10-21 23:49:03 -0300

    1014 Consumption

---
+++ Java/beginner/15-Consumption/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int totalDistance = scanner.nextInt();
+        double totalFuel = scanner.nextDouble();
+
+        double avarageConsumption = totalDistance / totalFuel;
+
+        System.out.printf("%.3f km/l\n", avarageConsumption);
+
+        scanner.close();
+    }
+}
