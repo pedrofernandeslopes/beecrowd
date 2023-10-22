5656714c0abc12d8b6798d0314d74551eb040ecf
Pedro Fernandes Lopes <pedroflopes1710@gmail.com>
2023-10-21 23:39:38 -0300

    1013 The Greatest

---
+++ Java/beginner/14-The Greatest/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int value1 = scanner.nextInt();
+        int value2 = scanner.nextInt();
+        int value3 = scanner.nextInt();
+
+        if (value1 > value2 && value1 > value3) {
+            System.out.println(value1 + " eh o maior");
+        } else if (value2 > value3) {
+            System.out.println(value2 + " eh o maior");
+        } else {
+            System.out.println(value3 + " eh o maior");
+        }
+        scanner.close();
+    }
+}
