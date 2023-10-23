3e51ac27c1965e1057876b9aca6199df3ff218db
Pedro Fernandes Lopes <pedroflopes1710@gmail.com>
2023-10-23 12:10:53 -0300

    1017 Fuel Spent

---
+++ Java/beginner/18-Fuel Spent/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int averageSpeed  = scanner.nextInt();
+        int time = scanner.nextInt();
+
+        double distance = averageSpeed * time;
+        
+        double fuel  = distance / 12;
+
+        System.out.printf("%.3f%n", fuel);
+        scanner.close();
+    }
+}
