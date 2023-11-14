ed6129c7309ccff82807a5e4d873a11afbfda149
pedro <pedroflopes1710@gmail.com>
2023-11-13 22:01:41 -0300

    1002 Area of a circle

--- javascript/beginner/03-area-of-a-circle.js
+++ javascript/beginner/03-area-of-a-circle.js
@@ -2,7 +2,9 @@ var input = require('fs').readFileSync('/dev/stdin', 'utf8');
 var lines = input.split('\n');
 
 var n = 3.14159;
+
 var raio = parseFloat(lines.shift());
+
 var area = n * (raio * raio);
 
 console.log(`A=${area.toFixed(4)}`);
