0e28c428182d3f7419ade1523e2ab6ede985c938
pedro <pedroflopes1710@gmail.com>
2024-06-17 15:22:30 -0300

    1003 Simple Sum

---
+++ src/beginner/SimpleSum/Main.java
@@ -0,0 +1,22 @@
+package beginner.SimpleSum;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+
+        int SOMA = A + B;
+
+        System.out.println("SOMA = " + SOMA);
+
+        scanner.close();
+    }
+}
+
