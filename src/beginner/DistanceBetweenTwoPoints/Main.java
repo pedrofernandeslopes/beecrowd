c037b5d5d18e140dd222f434ef60c00519c5ccfd
pedro <pedroflopes1710@gmail.com>
2024-06-18 15:17:19 -0300

    1015 Distance Between Two Points

---
+++ src/beginner/DistanceBetweenTwoPoints/Main.java
@@ -0,0 +1,22 @@
+package beginner.DistanceBetweenTwoPoints;
+
+import java.io.IOException;
+import java.util.Scanner;
+ 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        double x1 = sc.nextDouble();
+        double y1 = sc.nextDouble();
+        double x2 = sc.nextDouble();
+        double y2 = sc.nextDouble();
+
+        double distance = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+
+        System.out.printf("%.4f\n", distance);
+
+        sc.close();   
+    }
+ 
+}
